0891ce30932aa794888fdc58be8f9a21bb524ca7
5878794@qq.com <xf771026aa>
2018-05-08 10:06:27 +0800

    about css ok

---
+++ trunk/artSpace/pages/about/index.js
@@ -0,0 +1,49 @@
+const regeneratorRuntime = require('../../utils/runtime.js');
+const jq = require('../../utils/jq.js');
+const wxApp =require('../../utils/wxApp.js');
+const pageFn = require('../../utils/pagePublish');
+const data = require('../../utils/art_space_data');
+
+
+global.jq = jq;
+global.app = wxApp;
+
+Page({
+	data:{
+		loading_pre:10,
+		menu:[
+			{name:'HOME PAGE',url:'111'},
+			{name:'ART SHOW',url:'222'},
+			{name:'ARTIST',url:'333'},
+			{name:'GALLERY',url:'444'},
+			{name:'ABOUT AURA',url:'555'},
+			{name:'CONTACT',url:'666'}
+		],
+
+
+		banner:[],
+		text:[]
+	},
+	onLoad:function(){
+
+		let data = this.getData();
+		this.setData({
+			banner:data.banner,
+			text:data.text
+		});
+
+		// let newMenuData = [];
+		// this.data.menu1.map(rs=>{
+		// let new2Menu = [];
+		//    newMenuData.push(new2Menu);
+		//    rs.split('').map(text=>{
+		//    	new2Menu.push(text);
+		//    });
+		// });
+		// console.log(newMenuData)
+		// this.setData({menu:newMenuData});
+	},
+	getData(){
+		return data.about;
+	}
+});
