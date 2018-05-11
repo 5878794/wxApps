2ac8c7acba2c55ca830e76854349396640c83b40
5878794@qq.com <xf771026aa>
2018-05-11 16:23:04 +0800

    contact ok

--- trunk/artSpace/pages/gallery/index.js
+++ trunk/artSpace/pages/gallery/index.js
@@ -11,35 +11,71 @@ wxApp.ready({
 
 
 		list:[],
-		nowImgSrc:''
+		nowImgSrc:'',
+		imgStyle:'',
+		divStyle:''
 	},
+	now:0,
 	onLoad:function(){
 		Object.assign(this,all);
 		this.allInit('gallery');
 
 
 		let data = this.getData();
+		this.imgDatas = data;
 		this.setData({
 			list:data,
 			nowImgSrc:data[0]
 		});
 
-		// let newMenuData = [];
-		// this.data.menu1.map(rs=>{
-		// let new2Menu = [];
-		//    newMenuData.push(new2Menu);
-		//    rs.split('').map(text=>{
-		//    	new2Menu.push(text);
-		//    });
-		// });
-		// console.log(newMenuData)
-		// this.setData({menu:newMenuData});
+		this.device = wxApp.device();
+
+		this.bindEvent();
 	},
 	getData(){
 		return data.pavilion;
 	},
 	imageLoad(e){
-		console.log(e.detail.width)
-		console.log(e.detail.height)
+		let winW = this.device.windowWidth,
+			winH = this.device.windowHeight,
+			imgW = e.detail.width,
+			imgH = e.detail.height,
+			newImgW = winH*imgW/imgH,
+			left = (newImgW-winW)/2;
+		this.left = left;
+		this.setData({
+			imgStyle:'width:'+newImgW+'px;height:'+winH+'px;position:relative;left:-'+left+'px;'
+		});
+	},
+	bindEvent(){
+		let _this = this;
+		wx.onAccelerometerChange(function(res) {
+			if(_this.left<0){return;}
+
+			let left = -_this.left*res.x;
+			_this.setData({
+				divStyle:'transform:translateX('+left+'px)'
+			})
+		});
+
+		jq(this,'left_btn').tap(function(){
+			_this.now--;
+			_this.changeImg();
+		});
+		jq(this,'right_btn').tap(function(){
+			_this.now++;
+			_this.changeImg();
+		});
+	},
+	changeImg(){
+		let now = this.now,
+			all = this.data.list.length;
+		now = (now>=all)? 0 : now;
+		now = (now<0)? all-1 : now;
+		this.now = now;
+		this.setData({
+			nowImgSrc:this.imgDatas[now]
+		})
+
 	}
 });
