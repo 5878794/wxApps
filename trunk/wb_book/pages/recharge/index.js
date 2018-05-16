ee37136e63c6bea06400a8d5c438ddf61d89ff89
5878794@qq.com <xf771026aa>
2018-05-16 11:15:44 +0800

    recharge page ok

---
+++ trunk/wb_book/pages/recharge/index.js
@@ -0,0 +1,64 @@
+//index.js
+//获取应用实例
+const app = getApp();
+
+Page({
+	data: {
+		list:[
+			{
+				select:false,
+				money:'365元',
+				info:'包年会员',
+				send:''
+			},
+			{
+				select:false,
+				money:'50元',
+				info:'5000书币',
+				send:'送150元'
+			},
+			{
+				select:false,
+				money:'50元',
+				info:'5000书币',
+				send:'送150元'
+			},
+			{
+				select:true,
+				money:'150元',
+				info:'5000书币',
+				send:'送150元'
+			},
+			{
+				select:false,
+				money:'250元',
+				info:'5000书币',
+				send:'送150元'
+			},
+			{
+				select:false,
+				money:'50元',
+				info:'5000书币',
+				send:'送150元'
+			},
+			{
+				select:false,
+				money:'50元',
+				info:'5000书币',
+				send:'送150元'
+			}
+		],
+		rule:[
+			'充值活动开始时间。。。',
+			'阿迪看了风景啊看书的减肥卢卡斯就到风口浪尖啊大法离开的减肥卡拉斯京的饭卡的就是弗兰克阿斯顿计分卡世界的饭卡里舒服',
+			'充值活动开始时间。。。',
+			'充值活动开始时间。。。'
+		]
+
+	},
+	onLoad: function () {
+
+
+
+	}
+});
